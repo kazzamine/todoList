8944c31b3ac9ec40b8558676dfae32e4e8cd4545
kazzamine <aminkazzout@gmail.com>
2024-12-17 23:55:28 +0100

    add addtask redirect route

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use('/', taskRoutes);
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+app.get('/add.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'add.html'));
+});
 app.listen(port, () => {
     console.log(`Server status : Running`);
 });
